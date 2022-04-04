7555c7a6c0382b860250e94fbcf0b68f8dc8393d
ysYassine <66660853+ysYassine@users.noreply.github.com>
2022-04-04 18:32:28 +0100

    Updating the render API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import ReactDOM from "react-dom";
-import App from "./App";
+import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+createRoot(document.querySelector("#root")).render(<App />);
