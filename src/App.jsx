c19085c0b8a83325317ee0226d5283d4f465dfef
ysYassine <66660853+ysYassine@users.noreply.github.com>
2022-04-09 20:28:40 +0100

    Adding budgets functionality & UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,30 @@ import "./App.css";
 import Container from "react-bootstrap/esm/Container";
 import Header from "./components/header/Header";
 import BudgetCard from "./components/budget_card/BudgetCard";
-import { BudgetProvider } from "./contexts/BudgetsContext";
+import { useBudgets } from "./contexts/BudgetsContext";
 
 function App() {
+  const { budgets, getBudgetExpenses } = useBudgets();
   return (
-    <BudgetProvider>
-      <Container>
-        <Header></Header>
-        <div className="budget-container">
-          <BudgetCard name="Entertainment" amount={200} max={1000}></BudgetCard>
-          <BudgetCard name="Food" amount={510} max={500}></BudgetCard>
-          <BudgetCard
-            name="Uncategorized"
-            amount={200}
-            max={300}
-            showProgressBar={false}
-          ></BudgetCard>
-        </div>
-      </Container>
-    </BudgetProvider>
+    <Container>
+      <Header></Header>
+      <div className="budget-container">
+        {budgets.map((budget) => {
+          const amount = getBudgetExpenses(budget.id).reduce(
+            (total, expense) => total + expense.amount,
+            0
+          );
+          return (
+            <BudgetCard
+              key={budget.id}
+              name={budget.name}
+              amount={amount}
+              max={budget.max}
+            />
+          );
+        })}
+      </div>
+    </Container>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,24 @@
 import "./Header.css";
 
 import { Button } from "react-bootstrap";
+import { useState } from "react";
+import AddBudgetModal from "./../add_budget_modal/AddBudgetModal";
 
-const Header = () => {
+export default function Header() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
   return (
-    <div className="header">
-      <h1>Budgets</h1>
-      <Button variant="primary">Add budget</Button>
-      <Button variant="outline-primary">Add Expense</Button>
-    </div>
+    <>
+      <div className="header">
+        <h1>Budgets</h1>
+        <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+          Add budget
+        </Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </div>
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      ></AddBudgetModal>
+    </>
   );
-};
-
-export default Header;
+}
